b9c1724008acc25fb63b0803657ec9e44c686a6c
Elephant150 <dmytruk150@gmail.com>
2019-05-10 18:25:28 +0300

    full upgrade + delete + edit - design - view + some fix

--- config.php
+++ config.php
@@ -1,11 +1,51 @@
 <?php
-$connect = new PDO("mysql:host = ; dbname = ; charset = utf8", '', '');
-if (isset($_POST['text'])) {
-    $text = $_POST['text'];
-    $date = date('Y-m-d H:i:s');
-    $query = $connect->query(/** @lang text */ "INSERT INTO psaradise.todo (text, date) VALUES ('$text', '$date')");
+
+$task = "";
+$id = 0;
+$edit_task = false;
+
+$errors = "";
+
+$db = mysqli_connect('mysql.zzz.com.ua', 'psaradise', 'Psaradise1', 'psaradise');
+//server
+if (isset($_POST['submit'])) {
+    $task = $_POST['task'];
+
+    if (empty($task)) {
+        echo 'Error!!! Empty task';
+    } else {
+        mysqli_query($db, "INSERT INTO todo (task) VALUES ('$task')");
+        header('location: index.php');
+    }
+
 }
-if ((!empty($_POST['text']))) {
-    header('Location:http://todo-notebook.kl.com.ua/');
+//index
+if (isset($_GET['del_task'])) {
+    $id = $_GET['del_task'];
+
+    mysqli_query($db, "DELETE FROM todo WHERE id = $id");
+    header("location: index.php");
 }
-?>
+
+//server
+if (isset($_POST['edit'])) {
+    $task = $_POST['task'];
+    $id = $_POST['id'];
+
+    mysqli_query($db, "UPDATE todo SET task = '$task' WHERE id = $id");
+    header("location:index.php");
+}
+
+//------index-------------*************************
+if (isset($_GET['edit_task'])) {
+    $id = $_GET['edit_task'];
+
+    $edit_task = true;
+
+    $edit = mysqli_query($db, "SELECT * FROM todo WHERE id=$id");
+    $record = mysqli_fetch_array($edit);
+    $task = $record['task'];
+    $id = $record['id'];
+}
+//-------------------*************************
+$todo = mysqli_query($db, "SELECT * FROM todo");
--- index.php
+++ index.php
@@ -1,22 +1,60 @@
-<?php include 'header.php';
-include 'config.php';?>
-<div class="container">
-    <div class="row">
-        <div class="col-md">
-        </div>
-        <div class="col-md-8">
-            <form class="form" action="" method="POST">
-                <div class="form-group">
-                    <label for="exampleFormControlInput1">Add your item 🙃</label>
-                    <input type="text" name="text" class="form-control" id="exampleFormControlInput1" placeholder="Add your item" required>
-                </div><button type="submit" class="btn btn-primary mb-2">Add</button>
-            </form>
-            <div class="blockItem">
-                <?php include 'todo.php';?>
-            </div>
-        </div>
-        <div class="col-md">
-        </div>
-    </div>
-</div>
-<?php include 'footer.php'?>
+<?php
+include 'config.php';
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>T0D0List</title>
+</head>
+<body>
+<form action="index.php" method="post">
+    <input type="hidden" name="id" value="<?= $id; ?>">
+
+    <?php if (isset($errors)) { ?>
+        <p><?= $errors; ?></p>
+    <?php } ?>
+
+    <input type="text" name="task" class="taskInput" value="<?= $task; ?>">
+
+    <?php if($edit_task == false): ?>
+        <button type="submit" class="taskButton" name="submit">Add</button>
+
+    <?php else: ?>
+        <button type="submit" class="taskButton" name="edit">Edit</button>
+
+    <?php endif ?>
+</form>
+
+<table class="blockItem">
+    <thead>
+    <tr>
+        <th>№</th>
+        <th>Task</th>
+        <th>Delete</th>
+        <th>Edit</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php $i = 1;
+    while ($row = mysqli_fetch_array($todo)) { ?>
+        <tr>
+            <td><?= $i ?></td>
+            <td class="task"><?= $row['task']; ?></td>
+            <td class="delete">
+                <a href="index.php?del_task=<?= $row['id']; ?>">X</a>
+            </td>
+            <td class="edit">
+                <a href="index.php?edit_task=<?= $row['id']; ?>">E</a>
+            </td>
+        </tr>
+        <?php $i++;
+    } ?>
+    </tbody>
+</table>
+</body>
+</html>
